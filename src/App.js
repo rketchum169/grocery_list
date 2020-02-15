6a0ace8f9e76d655c773548ebcb5202e19c4f594
Ryan Ketchum <ryan@Ryans-MacBook-Pro-2.local>
2020-02-15 08:27:04 -0800

    basic functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = ()=> {
+  const [textareaValue, setTextareaValue] = useState ("")
+  const [notes, setNotes] = useState([])
+  const listItems = notes.map((note, i)=> <li key={i}>{note}</li>)
+
+  const storeEnteredText = (event)=> {
+    setTextareaValue(event.currentTarget.value)
+  }
+
+  const handleButtonClick = (event)=> {
+    setNotes([...notes, textareaValue])
+  }
+
   return (
     <div className="App">
-      <textarea></textarea>
-      <button>Save</button>
-      <ul></ul>
+      <textarea onChange={storeEnteredText}></textarea>
+      <button onClick={handleButtonClick}>Save</button>
+      <ul>{listItems}<button>Complete</button></ul>
     </div>
   );
 }

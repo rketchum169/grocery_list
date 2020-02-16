d0aff709e52e71d4dd7b7163f702e652e357d7a0
Ryan Ketchum <ryan@Ryans-MacBook-Pro-2.local>
2020-02-16 12:27:34 -0800

    Changing direction

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React, { useState } from 'react';
+import "./App.css";
+
+function Item({item, index}) {
+  return (
+    <div className="item">
+      { item.text }
+    </div>
+  )
+}
+
+function ItemForm({addItem}) {
+  const [textValue, setTextValue] = useState("")
+
+  return( 
+    <form onSubmit={handleButton}>
+      <input type="text" className="input" value={value} />
+    </form>
+  )
+}
 
 const App = ()=> {
-  const [textareaValue, setTextareaValue] = useState ("")
-  const [notes, setNotes] = useState([])
-  const listItems = notes.map((note, i)=> <li key={i}>{note}</li>)
+  const [items, setitems] = useState([])
+  
 
   const storeEnteredText = (event)=> {
     setTextareaValue(event.currentTarget.value)
@@ -13,11 +31,27 @@ const App = ()=> {
     setNotes([...notes, textareaValue])
   }
 
+  const completeItem = (note)=> {
+
+  }
+
+  const deleteItem = (note)=> {
+
+  }
+
+  const listItems = notes.map((note, i)=> 
+    <li key={i}>{note}{"  "}<button onClick={completeItem}>&#10004;</button><button onClick={deleteItem}>&#10006;</button></li>) 
+
+ 
   return (
     <div className="App">
-      <textarea onChange={storeEnteredText}></textarea>
-      <button onClick={handleButtonClick}>Save</button>
-      <ul>{listItems}<button>Complete</button></ul>
+      <textarea onChange={storeEnteredText}></textarea> 
+      <button id="save" onClick={handleButtonClick}>Save</button>
+      <ul className="item-list">
+        {items.map((item, i) => (
+          <Item key={index} index={index} item={item} />
+        ))}
+      </ul>
     </div>
   );
 }

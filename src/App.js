bac343c49f23f162a8b8eb580d4f29028732dfb8
Ryan Ketchum <ryan@Ryans-MacBook-Pro-2.local>
2020-02-16 12:42:12 -0800

    Cleaning up code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import "./App.css";
 
-function Item({item, index}) {
+function Item({item, i}) {
   return (
     <div className="item">
       { item.text }
@@ -12,46 +12,42 @@ function Item({item, index}) {
 function ItemForm({addItem}) {
   const [textValue, setTextValue] = useState("")
 
+  const handleButtonClick = (e)=> {
+    e.preventDefault()
+    if(!textValue) return
+    addItem(textValue)
+    setTextValue("")
+  }
+
   return( 
-    <form onSubmit={handleButton}>
-      <input type="text" className="input" value={value} />
+    <form onSubmit={handleButtonClick}>
+      <input type="text" className="input" value={textValue} onChange ={e => setTextValue (e.target.textValue)} />
     </form>
   )
 }
 
 const App = ()=> {
-  const [items, setitems] = useState([])
-  
+  const [items, setItems] = useState([])
 
-  const storeEnteredText = (event)=> {
-    setTextareaValue(event.currentTarget.value)
-  }
 
-  const handleButtonClick = (event)=> {
-    setNotes([...notes, textareaValue])
-  }
+  // const listItems = notes.map((note, i)=> 
+  //   <li key={i}>{note}{"  "}<button onClick={completeItem}>&#10004;</button><button onClick={deleteItem}>&#10006;</button></li>) 
 
-  const completeItem = (note)=> {
 
-  }
-
-  const deleteItem = (note)=> {
-
-  }
-
-  const listItems = notes.map((note, i)=> 
-    <li key={i}>{note}{"  "}<button onClick={completeItem}>&#10004;</button><button onClick={deleteItem}>&#10006;</button></li>) 
-
+  const addItem = text => {
+    const newItems = [...items, {text}]
+    setItems(newItems)
+  }  
  
   return (
     <div className="App">
-      <textarea onChange={storeEnteredText}></textarea> 
-      <button id="save" onClick={handleButtonClick}>Save</button>
-      <ul className="item-list">
+      <ItemForm addItem={addItem}/>
+      <ul>
         {items.map((item, i) => (
-          <Item key={index} index={index} item={item} />
+          <Item key={i} index={i} item={item} />
         ))}
       </ul>
+
     </div>
   );
 }
